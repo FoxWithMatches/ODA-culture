707729d93c3918596eea83eb94dc17e7d54b60db
Alisa Ts <freiheiit92@mail.ru>
2022-08-18 17:56:44 +0300

    burger

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 $(function() {
-    console.log("rabotaet");
 
-    /* Products https://kenwheeler.github.io/slick/ */
+/* Products https://kenwheeler.github.io/slick/ */
 
 let slider = $("#slickProducts");
 
@@ -13,6 +12,59 @@ slider.slick({
     arrows: false,
     dots: true
   });
+
+/* Header */
+
+let header = $("#header");
+let intro = $("#intro");
+let introH;
+let scrollPos = $(window).scrollTop();
+
+checkScroll(scrollPos, introH);
+
+$(window).on("scroll load resize", function() {
+  introH = intro.innerHeight();
+  scrollPos = $(this).scrollTop();
+
+  checkScroll(scrollPos, introH);
+  
+});
+
+function checkScroll(scrollPos, introH) {
+  if(scrollPos > introH) {
+    header.addClass("fixed");
+  } else {
+    header.removeClass("fixed");
+  }
+}
+
+$("[data-scroll]").on("click", function(event) {
+  event.preventDefault();
+
+  let elementID = $(this).data('scroll');
+  let elementOffset = $(elementID).offset().top;
+  nav.removeClass("show");
+
+  console.log(elementID)
+
+  $("html, body").animate({
+    scrollTop: elementOffset
+  }, 700)
+});
+
+/* navToggle */
+
+let nav = $("#nav");
+let navToggle = $("#navToggle");
+
+navToggle.on("click", function(event) {
+  event.preventDefault();
+
+  nav.toggleClass("show");
+
+});
+
+
 });
 
 

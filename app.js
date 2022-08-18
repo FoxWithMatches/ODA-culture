87c5f2c69c1a3db2d7f8d289865394e96a3fb41b
Alisa Ts <freiheiit92@mail.ru>
2022-08-18 12:23:09 +0300

    add slick js

---
+++ app.js
@@ -0,0 +1,19 @@
+$(function() {
+    console.log("rabotaet");
+
+    /* Products https://kenwheeler.github.io/slick/ */
+
+let slider = $("#slickProducts");
+
+slider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    dots: true
+  });
+});
+
+
+                  
